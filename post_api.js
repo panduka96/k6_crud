e662f83ee282eac4c57ff6add019bafb1c30dcc2
panduka wijekoon <panduka.wijekoon@grubtech.com>
2024-06-24 22:57:16 +0530

    Get & Post API performance test using k6

---
+++ post_api.js
@@ -0,0 +1,39 @@
+import http from 'k6/http'
+import { check } from 'k6'
+
+export let options = {
+    vus: 10,
+    duration: '10s',
+    thresholds: {
+        'checks': ['rate>0.95']
+    }
+}
+
+export default function () {
+
+    let url = 'https://run.mocky.io/v3/47565135-e942-4c4d-9818-6e4812045f39'
+
+    let headerParams = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    let payload = JSON.stringify({
+        email: "test@gmail.com",
+        password: "1234@#$"
+    })
+
+    const response = http.post(url, payload, headerParams)
+
+    check(response, {
+        'is status 200': (r) => r.status === 200,
+        'is message success': (res) => JSON.parse(res.body).message == "hello world"
+    })
+
+    let body = JSON.parse(response.body)
+    console.log('body is ', JSON.stringify(body))
+    console.log('message : ', body.message)
+    console.log('code : ', body.code)
+
+}
